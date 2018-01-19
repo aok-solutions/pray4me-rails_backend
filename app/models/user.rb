1b09bf0666a3fe2d0bf613932753ff60b1dbfd4d
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-01-18 21:28:25 -0500

    tags to categorize prayer requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :prayer_requests, dependent: :destroy
+  has_many :tags, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118055208) do
+ActiveRecord::Schema.define(version: 20180119022639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20180118055208) do
     t.index ["user_id"], name: "index_prayer_requests_on_user_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 20180118055208) do
   end
 
   add_foreign_key "prayer_requests", "users"
+  add_foreign_key "tags", "users"
 end
